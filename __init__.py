fc42d05beef836267445145d66c11dbbb6132024
Andrew Chepreghy <andris788@gmail.com>
2019-06-08 16:58:23 +0200

    added xgboost to the project

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ import pandas as pd
 import time as time
 
 from neural_network_classifier import run_neural_network
+from xgboost_classifier import run_xgboost
 
 df = pd.read_hdf('data/FAKE_train.h5')
-model = run_neural_network(df, batch_size=5, hidden_layers=[128, 128], n_epochs=5)
+# model = run_neural_network(df, batch_size=5, hidden_layers=[128, 128], n_epochs=5)
+model = run_xgboost(df)
---
+++ xgboost_classifier.py
@@ -0,0 +1,16 @@
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+
+
+
+def run_xgboost(df):
+  x_train = df.drop(columns={"Truth", "p_truth_E"})
+  y_train = df["Truth"]
+
+  model = XGBClassifier()
+  model.fit(x_train, y_train)
+
+  y_pred_train = model.predict(x_train)
+  predictions_train = [round(value) for value in y_pred_train]
+  accuracy_train = accuracy_score(y_train, predictions_train)
+  print("Accuracy on training set: %.2f%%" % (accuracy_train * 100.0))
