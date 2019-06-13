9cf3213f39db18502e3bf78f0df5ed7f964b1cfd
Andrew Chepreghy <andris788@gmail.com>
2019-06-13 10:56:05 +0200

    fixed neural network input

--- neural_network_classifier.py
+++ neural_network_classifier.py
@@ -12,29 +12,38 @@ from tensorflow.keras.utils import to_categorical
 
 from plotify import Plotify
 
+df = pd.read_pickle('word_2_grams.pkl')
+
+X = df['text'].tolist()
+y = df['author_one_hot'].tolist()
+
 plotify = Plotify()
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+X_train = np.array(X_train)
+X_test = np.array(X_test)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
 
 # define model
 model = Sequential()
-model.add(Dense(256, input_dim=X_train.shape[1], activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(256, input_dim=len(X_train[0]), activation='relu', kernel_initializer='he_uniform'))
 model.add(Dense(128, input_dim=256, activation='relu', kernel_initializer='he_uniform'))
 
-model.add(Dense(3, activation='softmax'))
-opt = SGD(lr=0.01, momentum=0.9)
+model.add(Dense(19, activation='softmax'))
+opt = SGD(lr=0.01, momentum=0.2)
 model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
+print('X_train.shape', X_train.shape)
 
-history = model.fit(X_train, y_train, validation_data=(X_train, y_test), epochs=100, verbose=0)
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=200, verbose=0)
 
 # evaluate the model
 _, train_acc = model.evaluate(X_train, y_train, verbose=0)
-_, test_acc = model.evaluate(X_train, y_test, verbose=0)
+_, test_acc = model.evaluate(X_test, y_test, verbose=0)
 print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
 # plot loss during training
 plt.subplot(211)
