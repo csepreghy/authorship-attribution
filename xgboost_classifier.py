ceac437ea75489a62fd2e86143fad880626778b8
Andrew Chepreghy <andris788@gmail.com>
2019-06-13 17:39:42 +0200

    changes to xgboost

--- xgboost_classifier.py
+++ xgboost_classifier.py
@@ -1,16 +1,20 @@
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
-
+import pandas as pd
 
 
 def run_xgboost(df):
-  x_train = df.drop(columns={"Truth", "p_truth_E"})
-  y_train = df["Truth"]
+  X_train = df['text']
+  y_train = df["author_one_"]
 
   model = XGBClassifier()
-  model.fit(x_train, y_train)
+  model.fit(X_train, y_train)
 
-  y_pred_train = model.predict(x_train)
+  y_pred_train = model.predict(X_train)
   predictions_train = [round(value) for value in y_pred_train]
   accuracy_train = accuracy_score(y_train, predictions_train)
   print("Accuracy on training set: %.2f%%" % (accuracy_train * 100.0))
+
+
+df = pd.read_pickle('word_2_grams.pkl')
+run_xgboost(df)
